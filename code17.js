1f066590248954cb5fee75a120717044c7a72571
student-BPSMV <sharmanandini294@gmail.com>
2026-09-09 18:51:09 +0530

    Initial commit

---
+++ code17.js
@@ -0,0 +1,31 @@
+//Practice Questions on strings
+
+// Remove numbers and special characters, keeping only letters
+
+// let text = "Hello123@World!";
+// let result = text.replace(/[^a-zA-Z]/g,"");
+// console.log(result);
+// let result = text.replace(/[0-9]/g,"");
+// console.log(result);
+
+// Check whether two strings are anagrams
+// Two strings are anagrams if they contain the same characters with the same frequencies, but possibly in a different order.
+
+// let str1 = "listen";
+// let str2 = "view";
+
+// let sorted1 = str1.split("").sort().join("");
+// let sorted2 = str2.split("").sort().join("");
+
+// if(sorted1 === sorted2){
+//     console.log("Anagrams");
+// }else{
+//     console.log("Not Anagrams");
+// }
+
+//Remove all vowels
+// let text = "Hello World";
+// let result = text.replace(/[aeiou]/g , "");
+// console.log(result);
+
+// Find the most frequently occurring character
